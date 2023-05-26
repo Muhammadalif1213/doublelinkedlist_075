838262b0cf1514ecc848649bc2099ebdc06efe72
Muhammadalif1213 <muhammadalif1213@gmail.com>
2023-05-26 20:46:02 +0700

    menambahkan fungsi hapus untuk menghapus node dalam list

--- doublelinkedlist/doublelinkedlist.cpp
+++ doublelinkedlist/doublelinkedlist.cpp
@@ -115,3 +115,17 @@ void DoubleLinkedList::traverse() {
 	}
 }
 
+void DoubleLinkedList::hapus() {
+	if (listEmpty()) {
+		cout << "\nlist is empty" << endl;
+
+	}
+	cout << "\nEnter the roll number of the student whose record is to be deleted: ";
+	int rollNo;
+	cin >> rollNo;
+	cout << endl;
+	if (DoubleLinkedList::deleteNode(rollNo) == false)
+		cout << "Record not found" << endl;
+	else
+		cout << "Record with roll number" << rollNo << "deleteted" << endl;
+}
